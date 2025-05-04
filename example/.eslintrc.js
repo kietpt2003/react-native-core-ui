0db982d14098dafa1aaecf6ed992c39d0e895996
kietpt2003 <tuankiet29012003@gmail.com>
2025-05-04 20:50:01 +0700

    Fix readme.md and code rules

--- example/.eslintrc.js
+++ example/.eslintrc.js
@@ -1,4 +1,9 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  ignorePatterns: ['metro.config.js', 'documents/*'],
+  rules: {
+    'react/jsx-filename-extension': 'off',
+    'react-hooks/exhaustive-deps': 'off',
+  },
 };
