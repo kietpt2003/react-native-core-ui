9f9f541098191632110ff1e824df8a41f0f7d71c
kietpt2003 <tuankiet29012003@gmail.com>
2025-05-05 01:07:37 +0700

    Update UI Sample

--- example/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  root: true,
-  extends: '@react-native',
-  ignorePatterns: ['metro.config.js', 'documents/*'],
-  rules: {
-    'react/jsx-filename-extension': 'off',
-    'react-hooks/exhaustive-deps': 'off',
-  },
-};
---
+++ example/RNCoreUISample/README.md
@@ -0,0 +1,4 @@
+This is a sample project, bootstrapped using [`@kietpt2003/react-native-core-ui`](https://github.com/kietpt2003/react-native-core-ui).
+
+# Getting Started
+
